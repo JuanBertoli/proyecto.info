9a7540c80b43672b2442c7229d0d269ee6c90208
JuanBertoli <bertolijuan777@gmail.com>
2023-12-06 22:33:48 -0300

    agregando modelo de base datos

--- blog/apps/posts/models.py
+++ blog/apps/posts/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 
@@ -9,3 +10,10 @@ class Categoria(models.Model):
     
     def __str__(self) -> str:
         return self.nombre
+class Post(models.Model):
+    nombre = models.CharField(max_length=50, null=False)
+    subtitulo = models.CharField(max_length=100, null=True, blank=True)
+    fecha = models.DateTimeField(auto_now_add=True)
+    texto = models.TextField(null=False)
+    activo = models.BooleanField(default=True)
+    
